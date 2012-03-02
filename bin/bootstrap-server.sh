619866e63b993def01db0213c9c49543831a5fde
Robbie Mackay <rm@robbiemackay.com>
2012-03-02 16:15:17 +1300

    Simplify bootstrap-server.sh so I know what's happening

--- bin/bootstrap-server.sh
+++ bin/bootstrap-server.sh
@@ -27,31 +27,4 @@ if [[ ! $INSTANCE =~ .*@.* ]]; then
     INSTANCE="$REMOTEUSER@$INSTANCE"
 fi
 
-
-exit_cleanly () {
-    nc_pid=`netstat -ntlp 2>/dev/null | grep 12345.*nc | awk '{print $7}' | cut -f 1 -d '/'`
-    if [ -n "$nc_pid" ]; then
-        kill $nc_pid;
-    fi
-
-    exit 0
-}
-
-trap exit_cleanly SIGINT SIGTERM
-
-SCRIPTPATH=$(dirname $(readlink -f $0))
-# Dodgy hax to send the file and execute it in one go, so we don't have to ssh
-# twice (and thus make the user enter the root pw twice)
-( nc -l 12345 < scripts/production-bootstrap.sh &
-  cd $SCRIPTPATH/.. &&
-  ssh -t -R12345:localhost:12345 $INSTANCE $SSH_OPTS "$sudo apt-get install -qq -y netcat > /dev/null && nc localhost 12345 > production-bootstrap.sh && $sudo sh production-bootstrap.sh $ROLES" )
-
-# This is an alternative way that also works, but the first thing the
-# production-bootstrap script should do is remove the authorized_keys file for
-# root if you do it this way.
-#( cd $SCRIPTPATH/.. &&
-#  ssh-copy-id $REMOTEUSER@$INSTANCE > /dev/null &&
-#  tar cz bin/prod-bootstrap.sh | ssh $REMOTEUSER@$INSTANCE tar xz &&
-#  ssh $REMOTEUSER@$INSTANCE sh bin/prod-bootstrap.sh $ROLES )
-
-exit_cleanly
+( scp $SSH_OPTS scripts/production-bootstrap.sh $INSTANCE: && ssh $SSH_OPTS $INSTANCE $sudo sh production-bootstrap.sh $ROLE )
